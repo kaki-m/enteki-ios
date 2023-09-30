c2e806d4fd43b1b2888edb77b74f1fe919d094f8
kaki-m <dskk0921@icoud.com>
2023-09-30 16:13:15 +0900

    Initial Commit

---
+++ enteki-ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  enteki-ios
+//
+//  Created by 柿崎愛斗 on 2023/09/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Initial commit")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ enteki-ios/enteki_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  enteki_iosApp.swift
+//  enteki-ios
+//
+//  Created by 柿崎愛斗 on 2023/09/30.
+//
+
+import SwiftUI
+
+@main
+struct enteki_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
