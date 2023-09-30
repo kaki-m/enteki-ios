0b6b76f04c883163edd78e9fbbde42e9a26541e3
kaki-m <dskk0921@icoud.com>
2023-09-30 16:37:40 +0900

    committest

--- enteki-ios/ContentView.swift
+++ enteki-ios/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Initial commit")
+            Text("Hello, world!")
         }
         .padding()
     }
